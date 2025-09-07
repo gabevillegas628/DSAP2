bc6ba00bbef7e92185a45f2df728689ebcaa9cd8
Gabe Villegas <gabevillegas@gmail.com>
2025-09-07 12:38:02 -0400

    Fix: had to rename AWS env variables

--- server/index.js
+++ server/index.js
@@ -111,9 +111,9 @@ const storage = multerS3({
 });
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_REGION
 });
 
 const fileFilter = (req, file, cb) => {
